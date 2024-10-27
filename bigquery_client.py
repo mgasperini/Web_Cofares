8775c4ba7d1a26d7963fdc095c784d30e1e966a4
mgasperini <matias.gasperini@gmail.com>
2024-10-27 17:22:11 +0100

    FEAT: obtiene productos y los devuelve en lista

--- bigquery_client.py
+++ bigquery_client.py
@@ -1,26 +1,80 @@
 from google.cloud import bigquery
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Carga las variables desde .env al entorno
+
+# Ahora puedes acceder a las variables de entorno
+project_id = os.getenv("GOOGLE_CLOUD_PROJECT")
 
 def get_products(prompt):
-    client = bigquery.Client()
+    client = bigquery.Client(project=project_id)
     query = """
-        SELECT codigo_web, informacion_producto->>'$.nombre' AS nombre,
-               informacion_producto->>'$.descripcion' AS descripcion,
-               informacion_producto->>'$.modo_implementacion' AS modo_implementacion,
-               informacion_producto->>'$.imagen_url' AS imagen_url
-        FROM `tu_proyecto.parafarmacia_data.productos`
-        WHERE informacion_producto LIKE '%{}%'
-        LIMIT 5
+    WITH QueryEmbedding AS (
+      SELECT
+        ml_generate_embedding_result AS query_embedding
+      FROM
+        ML.GENERATE_EMBEDDING(
+          MODEL `dataton-2024-team-01-cofares.datos_cofares.text_embedding`,
+          (SELECT @prompt AS content),  -- Aquí usamos el parámetro
+          STRUCT(TRUE AS flatten_json_output, 'RETRIEVAL_QUERY' AS task_type)
+        )
+    )
+    SELECT
+      d.material_final AS nombre,
+      JSON_EXTRACT_SCALAR(d.informacion_producto, "$.txt_mas_informacion_del_producto") AS descripcion,
+      JSON_EXTRACT_SCALAR(d.informacion_producto, "$.txt_instrucciones_de_uso") AS modo_implementacion,
+      d.codigo_web,
+      d.URI_primera_img,
+      d.codigo_nacional,
+      ML.DISTANCE(
+        qe.query_embedding,
+        e.ml_generate_embedding_result,
+        'COSINE'
+      ) AS distance_to_query
+    FROM
+      `dataton-2024-team-01-cofares.datos_cofares.data` AS d
+    INNER JOIN
+      `dataton-2024-team-01-cofares.datos_cofares.SalidaEmbeddings_test` AS e
+      ON d.codigo_web = e.codigo_web
+    INNER JOIN QueryEmbedding AS qe
+      ON TRUE
+    ORDER BY
+      distance_to_query
+    LIMIT 5;
     """.format(prompt)
-    query_job = client.query(query)
-    results = query_job.result()
+    # Configura el parámetro para el prompt
+    job_config = bigquery.QueryJobConfig(
+        query_parameters=[
+            bigquery.ScalarQueryParameter("prompt", "STRING", prompt)
+        ]
+    )
 
+    query_job = client.query(query, job_config=job_config)
+    results = query_job.result()
+    
     products = []
     for row in results:
+
+        descripcion = row.descripcion
+        if not row.descripcion:
+            descripcion = '-'
+        
+        modo_implementacion = row.modo_implementacion
+        if not row.modo_implementacion:
+            modo_implementacion = '-'
+
+
+        # Cambia la URL si es necesario
+        imagen_url = row.URI_primera_img 
+        if imagen_url and imagen_url.startswith('gs:/'):
+            imagen_url = imagen_url.replace('gs://dataton-2024-team-01-cofares-datastore/imagenes/', 'https://storage.googleapis.com/dataton-2024-team-01-cofares-datastore/imagenes/reto_cofares/')
         products.append({
             "codigo_web": row.codigo_web,
             "nombre": row.nombre,
-            "descripcion": row.descripcion,
-            "modo_implementacion": row.modo_implementacion,
-            "imagen_url": row.imagen_url
+            "codigo_nacional": row.codigo_nacional,
+            "descripcion": descripcion,
+            "modo_implementacion": modo_implementacion,
+            "imagen_url": imagen_url
         })
     return products
